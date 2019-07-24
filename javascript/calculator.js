e81c11ae260041a1e8d873360bdae650c300b94b
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-07-24 10:49:00 +0100

    applying bem

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,20 +1,20 @@
 // set default value to 0
-document.getElementById('display').innerHTML = "0";
+document.querySelector('.calculator__display').innerHTML = "0";
 
 // make numbers display on screen
-const buttons = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".calculator__btn");
 
 for (const button of buttons) {
     button.addEventListener('click', function displayBtn(buttons) {
         let answer = button.innerHTML;
-        document.getElementById('display').innerHTML = answer;
+        document.querySelector('.calculator__display').innerHTML = answer;
         return answer;
     })
 }
 
 // make buttons clear 
-document.querySelector('.clear-btn').addEventListener('click', function () {
-    document.getElementById('display').innerHTML = "0";
+document.querySelector('.calculator__btn--clear').addEventListener('click', function () {
+    document.querySelector('.calculator__display').innerHTML = "0";
 });
 
 // make numbers display when they are pressed
